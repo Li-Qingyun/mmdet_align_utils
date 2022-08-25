34a05067ec3be43988c3f5b4c70316322c645d38
QingyunLi <962537281@qq.com>
2022-08-25 16:51:24 +0800

    Update all configs of DINO

---
+++ all_dino_configs/aligned_dino_5scale_swin-l-p4-w12_16x1_12e_coco.py
@@ -0,0 +1,13 @@
+_base_ = 'dino_5scale_r50_16x1_12e_coco.py'
+model = dict(
+    backbone=dict(norm_cfg=dict(type='FrozenBatchNorm2d')),
+    neck=dict(conv_cfg=dict(type='Conv2d', bias=True)),
+    bbox_head=dict(num_classes=91))
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(continuous_categories=False),
+    val=dict(continuous_categories=False),
+    test=dict(continuous_categories=False))
---
+++ all_dino_configs/dino_4scale_swin-t-p4-w7_8x2_12e_coco.py
@@ -0,0 +1,31 @@
+_base_ = './dino_4scale_r50_8x2_12e_coco.py'
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth'  # noqa
+model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='SwinTransformer',
+        embed_dims=96,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.2,
+        patch_norm=True,
+        out_indices=(1, 2, 3),
+        with_cp=False,
+        convert_weights=True,
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
+    neck=dict(in_channels=[192, 384, 768]))
+# optimizer
+optimizer = dict(
+    paramwise_cfg=dict(
+        custom_keys={
+            'backbone': dict(lr_mult=0.1),
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
